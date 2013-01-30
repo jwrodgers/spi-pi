9b22e5fb5d5b80eae6ee45284220e72737aca166
jwrodgers <raspberrypi2@gmail.com>
2013-01-30 22:50:00 +0000

    Update dummyDataStream.c

--- dummyDataStream.c
+++ dummyDataStream.c
@@ -1,8 +1,13 @@
 // testwiringPI file
 
-int wiringPiSPIDataRW ( int channel, unsigned char* &buffer, int length )
+int wiringPiSPIDataRW ( int channel, unsigned char *buffer, int length )
 {
+  int i;
   
+  for(i=0; i<length; i++){
+    *buffer = (char)i;
+    buffer++;
+  }
   
   return 0;
 }
