3c01da45ac3e93e481f09ae912e5a6a53bb0a172
jwrodgers <raspberrypi2@gmail.com>
2013-01-30 21:35:43 +0000

    Update spigraph.c
    Reformatted a bit. Some clean up comments needed.

--- spigraph.c
+++ spigraph.c
@@ -1,7 +1,10 @@
-// Run this on the PI.
-// Needs wiringPI installed to work correctly
-// Outputs the data received as values from the A/D, Filters 7E padding/escapes inserted at PIC side
-//Change to see if this affects the master branch or the cleanup!
+// Program communicates with PCB-0001-0A Raspberry Pi Peripheral. It takes data from the board and creates a file
+// that can be plotted easily using dygraphs web library.
+//
+// This program runs in the Raspberry Pi, and requires the wiringPi Library to be installed on the PI
+// This program can filter 7E Escapes sequences produced by the PIC code
+// It assembles 2 byte packets into a complete value from the A/D on the PIC
+// 
 
 #include <stdio.h>
 #include <stdint.h>
@@ -10,99 +13,83 @@
 #include <time.h>
 #include <sys/timeb.h>
 
-//Strips out 0x7E symbols from data stream
+int main(void){
 
+	struct tm *tm_ptr;
+	time_t the_time;
 
-void main(void){
+    	(void) time(&the_time);
+    	tm_ptr = gmtime(&the_time);
 
-    struct tm *tm_ptr;
-    time_t the_time;
+	/* fast timeb function for milliseconds */
+    	struct timeb tmb;
+    	struct timeb starttime;
+    	struct timeb stoptime;
 
-    (void) time(&the_time);
-    tm_ptr = gmtime(&the_time);
-
-
-/* fast timeb function for milliseconds */
- 
-    struct timeb tmb;
-    struct timeb starttime;
-    struct timeb stoptime;
-
-
-
-unsigned char buffer[4096];
-unsigned char data[4096];
-int i;
-for(i=0;i<4096;i++){
-  buffer[i]=0xAA;
-	data[i]=0x55;
-}
-
-/*
-buffer[0]=0;
-buffer[1]=1;
-buffer[2]=2;
-buffer[3]=3;
-buffer[4]=4;
-buffer[5]=0xAA;
-buffer[6]=0x55;
-
-*/
-int channel=0;
-
-
-if (wiringPiSPISetup (channel, 500000) < 0)
-  fprintf (stderr, "SPI Setup failed!\n");
-
-
-
-ftime(&starttime);
-int j=0;
-for(j=0; j<1; j++){
-
-       wiringPiSPIDataRW ( channel, &buffer, 4096) ;
-
-}
-        ftime(&stoptime);
-
-//copy buffer to data removing 0x7E symbols
-int k=0;
-for(j=0; j<4096; j++)
-{
-	if(buffer[j]==0x7E)continue;
-
-	if(buffer[j]==0x7D){
-		if(buffer[j+1]==0x5E){
-			data[k]=0x7E;
-			j++;
-			k++;
-			continue;
-		}
-		if(buffer[j+1]==0x5D){
-			data[k]=0x7D;
-                        j++;
-                        k++;
-                        continue;
-		}
+	unsigned char buffer[4096];
+	unsigned char data[4096];
+	int i;
+	for(i=0;i<4096;i++){
+	  	buffer[i]=0xAA;
+		data[i]=0x55;
 	}
-	data[k]=buffer[j];
-	k++;
-}
-
-
-
-
-
-for(i=0;i<k;i+=2){
 	
+	/*
+	buffer[0]=0;
+	buffer[1]=1;
+	buffer[2]=2;
+	buffer[3]=3;
+	buffer[4]=4;
+	buffer[5]=0xAA;
+	buffer[6]=0x55;
+	*/
+	
+	int channel=0;
+	
+	if (wiringPiSPISetup (channel, 500000) < 0)
+		fprintf (stderr, "SPI Setup failed!\n");
+	
+	ftime(&starttime);
+	
+	int j=0;
+	for(j=0; j<1; j++){
+	       wiringPiSPIDataRW ( channel, &buffer, 4096) ;
+	}
+	
+	ftime(&stoptime);
+	
+	//copy buffer to data removing 0x7E symbols
+	int k=0;
+	for(j=0; j<4096; j++)
+	{
+		if(buffer[j]==0x7E)continue;	//Cleanup needed, multiple if's so may use switch ???
+						// What are j & k ? Better small variable names needed, bi? di? for
+						// buffer index, data index.
+						
+		if(buffer[j]==0x7D){
+			if(buffer[j+1]==0x5E){
+				data[k]=0x7E;
+				j++;
+				k++;
+				continue;
+			}	
+			if(buffer[j+1]==0x5D){
+				data[k]=0x7D;
+	                        j++;
+	                        k++;
+	                        continue;
+			}	
+		}
+		data[k]=buffer[j];
+		k++;
+	}
+	
+	for(i=0;i<k;i+=2){
 	//Uncomment for DEBUG:
-//	printf("%d, 0x%X\n", i, data[i]);
-//	printf("%d, 0x%X     ", i+1, data[i+1]);	
-
-
-	printf("%d,%d\n", i, data[i]*256+data[i+1]);
-}
-
-
-
+	//	printf("%d, 0x%X\n", i, data[i]);
+	//	printf("%d, 0x%X     ", i+1, data[i+1]);	
+		printf("%d,%d\n", i, data[i]*256+data[i+1]);
+	}
+	
+	return (0);
 }
