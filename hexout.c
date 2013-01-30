cb743cca776f4705b751866766e3169485a3a63c
jwrodgers <raspberrypi2@gmail.com>
2013-01-30 21:14:08 +0000

    Update hexout.c
    Just changed some formatting

--- hexout.c
+++ hexout.c
@@ -29,45 +29,44 @@ void main(void){
 
 
 
-unsigned char buffer[4096];
-int i;
-for(i=0;i<4096;i++){
-  buffer[i]=0xAA;
-}
+	unsigned char buffer[4096];
+	int i;
+	for(i=0;i<4096;i++){
+	  buffer[i]=0xAA;
+	}
+	
+	/*
+	buffer[0]=0;
+	buffer[1]=1;
+	buffer[2]=2;
+	buffer[3]=3;
+	buffer[4]=4;
+	buffer[5]=0xAA;
+	buffer[6]=0x55;
+	
+	*/
+	int channel=0;
+	
+	
+	if (wiringPiSPISetup (channel, 500000) < 0)
+	  fprintf (stderr, "SPI Setup failed!\n");
+	
+	printf("\n\nEntering SPI Comms:\n");
+	
+	ftime(&starttime);
+	int j=0;
+	for(j=0; j<1; j++){
+	       wiringPiSPIDataRW ( channel, &buffer, 4096) ;
+	}
+	
+	ftime(&stoptime);
+	printf("seconds difference: %ld.%ld\n", stoptime.time-starttime.time);
+	printf("millis difference: %ld\n" , stoptime.millitm-starttime.millitm);
 
-/*
-buffer[0]=0;
-buffer[1]=1;
-buffer[2]=2;
-buffer[3]=3;
-buffer[4]=4;
-buffer[5]=0xAA;
-buffer[6]=0x55;
-
-*/
-int channel=0;
-
-
-if (wiringPiSPISetup (channel, 500000) < 0)
-  fprintf (stderr, "SPI Setup failed!\n");
-
-    printf("\n\nEntering SPI Comms:\n");
-
-
-ftime(&starttime);
-int j=0;
-for(j=0; j<1; j++){
-
-       wiringPiSPIDataRW ( channel, &buffer, 4096) ;
-
-}
-        ftime(&stoptime);
-        printf("seconds difference: %ld.%ld\n", stoptime.time-starttime.time);
-        printf("millis difference: %ld\n" , stoptime.millitm-starttime.millitm);
-// Show each character as recieved
-for(i=0;i<4096;i++){
-	printf("%d,0x%X\n", i, buffer[i]);
-}
+	// Show each character as recieved
+	for(i=0;i<4096;i++){
+		printf("%d,0x%X\n", i, buffer[i]);
+	}
 
 
 
